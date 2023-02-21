fe9f0b9ca93cac2967d36879d7b9ebb295057759
tobias-firth <tobyfirth@btinternet.com>
2023-02-21 15:20:43 +0000

    add code

---
+++ assets/js/logic.js
@@ -0,0 +1,86 @@
+// Show question div when start button is pressed
+
+var start = document.getElementById("start");
+var startScreen = document.getElementById("start-screen");
+var questionDisplay = document.getElementById("questions");
+var time = document.getElementById("time");
+var endScreen = document.getElementById("end-screen");
+var finalScoreEl = document.getElementById("final-score");
+
+
+var questionEle =  document.getElementById("question-title");
+var firstOptionEle =  document.getElementById("option-one");
+var secondOptionEle = document.getElementById("option-two");
+var thirdOptionEle = document.getElementById("option-three");
+var fourthOptionEle = document.getElementById("option-four");
+
+var index = 0
+var timeLeft = 75;
+time.innerHTML = timeLeft;
+var quizTimer = 0;
+var initials = ''
+var finalScore =
+
+start.addEventListener("click", displayQuestions);
+
+function displayQuestions() {
+    questionDisplay.setAttribute("style", "display:block; background-color:red; height:400px; width:100%");
+    startScreen.remove();
+    var quizTimer = setInterval(function(){
+        if(timeLeft <= 0){
+          clearInterval(quizTimer);
+          time.innerHTML = "Finished";
+        } else {
+          time.innerHTML = timeLeft
+        }
+        timeLeft -= 1;
+      },1000);
+}
+
+questionEle.innerHTML = questions[0].question;
+firstOptionEle.innerHTML = questions[0].option1;
+secondOptionEle.innerHTML = questions[0].option2;
+thirdOptionEle.innerHTML = questions[0].option3;
+fourthOptionEle.innerHTML = questions[0].option4;
+
+// If the answer if the first option, clicking that button will produce the next question
+
+firstOptionEle.addEventListener("click", checkAnswer);
+secondOptionEle.addEventListener("click", checkAnswer);
+thirdOptionEle.addEventListener("click", checkAnswer);
+fourthOptionEle.addEventListener("click", checkAnswer);
+
+function checkAnswer(){
+  var choice = event.target.innerHTML
+  console.log(choice)
+  if (choice !== questions[index].correctAnswer){
+    timeLeft = timeLeft - 10
+    nextQuestion()
+  } 
+  else {
+    nextQuestion()
+  }
+  
+}
+
+function nextQuestion() {
+  if (index < 9){  
+  index ++ ;
+    questionEle.innerHTML = questions[index].question;
+    firstOptionEle.innerHTML = questions[index].option1;
+    secondOptionEle.innerHTML = questions[index].option2;
+    thirdOptionEle.innerHTML = questions[index].option3;
+    fourthOptionEle.innerHTML = questions[index].option4;
+    // console.log(index);
+  }
+  else
+  {
+    endScreen.setAttribute("style", "display:block; background-color:blue; height:400px; width:100%");
+    questionDisplay.remove();
+    finalScoreEl.innerHTML = timeLeft;
+    finalScore = timeLeft
+    console.log(finalScore)
+    time.innerHTML = "Finished";
+    timeLeft = 0;
+  }
+}
---
+++ assets/js/questions.js
@@ -0,0 +1,84 @@
+// Store questions as an array of objects
+var questions = [
+    {
+        question:"Which of these elements in HTML can be used for making a text bold?",
+        option1:"&lta&gt",
+        option2:"&ltpre&gt",
+        option3:"&ltbr&gt",
+        option4:"&ltb&gt",
+        correctAnswer: "&ltb&gt"
+    },
+    {
+        question:"Which tag do we use to define the options present in the drop-down selection lists?",
+        option1: "&ltlist&gt",
+        option2: "&ltoption&gt",
+        option3: "&ltdropdown",
+        option4: "&ltselect&gt",
+        correctAnswer: "&ltoption&gt"
+    },
+    {
+        question:"In HTML, we use the &lthr&gt tag for ___________.",
+        option1:"horizontal ruler",
+        option2:"new line",
+        option3:"new paragraph",
+        option4:"vertical ruler",
+        correctAnswer:'horizontal ruler'
+    },
+    {
+        question: "In HTML, the tags are __________",
+        option1:"in upper case",
+        option2:"case-sensitive",
+        option3:"in lowercase",
+        option4:"not case sensitive",
+        correctAnswer:"not case sensitive"
+    },
+    {
+        question:" Which tag is used in HTML5 for the initialization of the document type?",
+        option1:"&ltDoctype HTML&gt",
+        option2:"&lt!DOCTYPE html&gt",
+        option3:"&ltDoctype&gt",
+        option4:"&lt\Doctype html&gt",
+        correctAnswer:"&lt!DOCTYPE html&gt"
+    },
+    {
+        question:"Which of these tags helps in the creation of a drop-down box or a combo box?",
+        option1:"&ltinput type = “dropdown”&gt",
+        option2:"&ltlist&gt",
+        option3:"&ltul&gt",
+        option4:"&ltselect&gt",
+        correctAnswer:"&ltselect&gt"
+    },
+    {
+        question:"Which HTML tag do we use to display text along with a scrolling effect?",
+        option1:"&ltdiv&gt",
+        option2:"&ltscroll&gt",
+        option3:"&ltmarquee&gt",
+        option4:"none of the above",
+        correctAnswer:"&ltmarquee&gt"
+    },
+    {
+        question:"In HTML, how do we insert an image?",
+        option1:"&ltimg src = “jtp.png” /&gt",
+        option2:"&ltimg href = “jtp.png” /&gt",
+        option3:"&ltimg link = “jtp.png” /&gt",
+        option4:"&ltimg url = “jtp.png” /&gt",
+        correctAnswer:"&ltimg src = “jtp.png” /&gt"
+    },
+    {
+        question: "How to create a hyperlink in HTML?",
+        option1: "&lta link = “www.thinkandlearn.com”&gt thinkandlearn.com &lt/a&gt",
+        option2:"&lta&gt www.thinkandlearn.com &ltthinkandlearn.com /a&gt",
+        option3:"&lta href = “www.thinkandlearn.com”&gt thinkandlearn.com &lt/a&gt",
+        option4:"&lta url = “www.thinkandlearn.com” thinkandlearn.com /a&gt",
+        correctAnswer:"&lta href = “www.thinkandlearn.com”&gt thinkandlearn.com &lt/a&gt"
+    },
+    {
+        question:"Which one is the HTML document’s root tag?",
+        option1:"&lthead&gt",
+        option2:"&ltbody&gt",
+        option3:"&lttitle&gt",
+        option4:"&lthtml&gt",
+        correctAnswer:"&lthtml&gt"
+    },
+]
+
